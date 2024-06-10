7947318da4f6c87cb50a67c47cb13ca67fbfd291
Abdul Halim Daud <halim.daud@hotmail.com>
2024-06-10 12:41:37 +0800

    update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,5 +19,4 @@
 # git clone https://github.com/jerrykuku/luci-app-argon-config.git
 
 # patch for libfring which causes build to fail https://github.com/openwrt/packages/issues/23621
-mkdir -p feeds/packages/libs/libpfring/patches/
-[ -e $GITHUB_WORKSPACE/999-issue-23621.patch ] && cp $GITHUB_WORKSPACE/999-issue-23621.patch feeds/packages/libs/libpfring/patches/
+[ -e "$GITHUB_WORKSPACE"/999-issue-23621.patch ] && mkdir -p feeds/packages/libs/libpfring/patches/ && cp "$GITHUB_WORKSPACE"/999-issue-23621.patch feeds/packages/libs/libpfring/patches/
